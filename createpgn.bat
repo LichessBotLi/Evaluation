ba6fe79d63ae294f29cd803e22c88e94b2a371d1
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2025-07-03 00:16:22 +0530

    Update createpgn.bat

--- createpgn.bat
+++ createpgn.bat
@@ -1,6 +1,6 @@
 @echo off
 call polyglot_tolerant dump-book -bin Optimus2502.bin -color white -out white-moves.txt
 call polyglot_tolerant dump-book -bin Optimus2502.bin -color black -out black-moves.txt
-call convertcerebellum.py
+call convert-cerebellum.py
 del white-moves.txt
 del black-moves.txt
