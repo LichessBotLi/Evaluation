5e2e24d11a4e73283cc453df6c1aa3625a00e5a0
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2025-07-03 18:17:49 +0530

    Update createpgn.bat

--- createpgn.bat
+++ createpgn.bat
@@ -1,10 +1,15 @@
 @echo off
 
+:: Set dummy engine
+set ENGINE_COMMAND=cmd.exe /c exit 0
 
+:: Call PolyGlot Tolerant with dummy engine
 call polyglot_tolerant dump-book -bin Optimus2502.bin -color white -out white-moves.txt
 call polyglot_tolerant dump-book -bin Optimus2502.bin -color black -out black-moves.txt
 
+:: Convert to PGN
 call convert-cerebellum.py
 
+:: Cleanup
 del white-moves.txt
 del black-moves.txt
